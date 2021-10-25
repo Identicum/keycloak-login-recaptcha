891afc88cc435dde95549d10c148db0e0435dcc1
osorionicolas <osorionicolas95@gmail.com>
2021-10-25 18:47:21 -0300

    Added httpStats and the possibility to login even if the recaptcha validation ends with a timeout (#2)
    * Added timeout configuration for recaptcha validation
    
    * Added log messages and constants for default values
    
    * Added configs to realm.json
    
    * Added httpStats
    
    * removed unnecessary variable
    
    * merged workflows
    
    * updated pom.xml to add plugins' versions to remove warnings during the build process
    
    Co-authored-by: Nicolas Osorio <osorionicolas95@gmail.com>
    Co-authored-by: ggallard <ggallard@identicum.com>

--- src/main/java/com/identicum/keycloak/recaptcha/RecaptchaUsernamePasswordForm.java
+++ src/main/java/com/identicum/keycloak/recaptcha/RecaptchaUsernamePasswordForm.java
@@ -1,12 +1,5 @@
 package com.identicum.keycloak.recaptcha;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.jboss.logging.Logger;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.Authenticator;
@@ -15,50 +8,55 @@ import org.keycloak.events.Details;
 import org.keycloak.forms.login.LoginFormsProvider;
 import org.keycloak.models.AuthenticatorConfigModel;
 import org.keycloak.models.utils.FormMessage;
-import org.keycloak.services.ServicesLogger;
 import org.keycloak.services.messages.Messages;
 import org.keycloak.services.validation.Validation;
-import org.keycloak.util.JsonSerialization;
 
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import java.io.InputStream;
 import java.util.*;
 
-public class RecaptchaUsernamePasswordForm extends UsernamePasswordForm implements Authenticator{
+public class RecaptchaUsernamePasswordForm extends UsernamePasswordForm implements Authenticator {
 	public static final String G_RECAPTCHA_RESPONSE = "g-recaptcha-response";
-	public static final String SITE_KEY = "site.key";
+	public static final String SITE_KEY = "siteKey";
 	public static final String SITE_SECRET = "secret";
+
 	private static final Logger logger = Logger.getLogger(RecaptchaUsernamePasswordForm.class);
 
 	private String siteKey;
 
 	@Override
 	protected Response createLoginForm( LoginFormsProvider form ) {
-		logger.info("Creating login form with recaptcha for site key " + siteKey);
-		form.setAttribute("recaptchaRequired", true);
-		form.setAttribute("recaptchaSiteKey", siteKey);
+		logger.infov("Creating login form");
+		if(siteKey != null) {
+			logger.debugv("For site key " + siteKey);
+			form.setAttribute("recaptchaRequired", true);
+			form.setAttribute("recaptchaSiteKey", siteKey);
+		}
 		return super.createLoginForm( form );
 	}
 
 	@Override
 	public void authenticate(AuthenticationFlowContext context) {
-		logger.info("Starting authentication flow");
+		RecaptchaUsernamePasswordFormFactory.createRestHandlerAndSetStats(context.getAuthenticatorConfig().getConfig());
+
+		logger.infov("Starting authentication flow");
 		context.getEvent().detail(Details.AUTH_METHOD, "auth_method");
 		AuthenticatorConfigModel captchaConfig = context.getAuthenticatorConfig();
 		LoginFormsProvider form = context.form();
-		logger.info("Verifying recaptcha configuration");
+		logger.infov("Verifying recaptcha configuration");
+		String isRecaptchaConfAvailableMsg = "Recaptcha configuration is not available";
 
 		if (captchaConfig != null && captchaConfig.getConfig() != null
 				&& captchaConfig.getConfig().get(SITE_KEY) != null
 				&& captchaConfig.getConfig().get(SITE_SECRET) != null) {
-			logger.info("Recaptcha configuration is available");
+			isRecaptchaConfAvailableMsg = "Recaptcha configuration is available";
 			siteKey = captchaConfig.getConfig().get(SITE_KEY);
 			form.setAttribute("recaptchaRequired", true);
 			form.setAttribute("recaptchaSiteKey", siteKey);
 		}
 
-		logger.debug("Calling authenticate method from parent class");
+		logger.infov(isRecaptchaConfAvailableMsg);
+		logger.debugv("Calling authenticate method from parent class");
 		super.authenticate(context);
 	}
 
@@ -66,57 +64,24 @@ public class RecaptchaUsernamePasswordForm extends UsernamePasswordForm implemen
 	public void action(AuthenticationFlowContext context) {
 		MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
 		List<FormMessage> errors = new ArrayList<>();
-		boolean success = false;
 		context.getEvent().detail(Details.AUTH_METHOD, "auth_method");
 		String captcha = formData.getFirst(G_RECAPTCHA_RESPONSE);
-		logger.info("Recaptcha response from form data: " + captcha);
+		logger.debugv("Recaptcha response from form data: " + captcha);
 
 		if (!Validation.isBlank(captcha)) {
 			AuthenticatorConfigModel captchaConfig = context.getAuthenticatorConfig();
 			String secret = captchaConfig.getConfig().get(SITE_SECRET);
-			logger.info("Validating recaptcha response");
-			success = validateRecaptcha(context, success, captcha, secret);
+			logger.infov("Validating recaptcha response");
+			boolean success = RecaptchaUsernamePasswordFormFactory.restHandler.validateRecaptcha(context, captcha, secret);
 			if (!success) {
 				errors.add(new FormMessage(null, Messages.RECAPTCHA_FAILED));
-				logger.info("Removing recaptcha response");
+				logger.infov("Removing recaptcha response");
 				formData.remove(G_RECAPTCHA_RESPONSE);
+				return;
 			}
 		}
-		logger.debug("Calling action method from parent class");
+		logger.infov("Calling action method from parent class");
 		super.action(context);
-	}
-
-	protected boolean validateRecaptcha(AuthenticationFlowContext context, boolean success, String captcha, String secret) {
-		HttpClient httpClient = HttpClients.createDefault();
-		String uri = "https://www.google.com/recaptcha/api/siteverify";
-		HttpPost post = new HttpPost(uri);
-		List<NameValuePair> formparams = new LinkedList<>();
-		formparams.add(new BasicNameValuePair("secret", secret));
-		formparams.add(new BasicNameValuePair("response", captcha));
-		formparams.add(new BasicNameValuePair("remoteip", context.getConnection().getRemoteAddr()));
-
-		try {
-			UrlEncodedFormEntity form = new UrlEncodedFormEntity(formparams, "UTF-8");
-			post.setEntity(form);
-			logger.info("Executing request to " + uri);
-			logger.debug("Executing request with parameters response: " + captcha + " and remoteip: " + context.getConnection().getRemoteAddr());
-			HttpResponse response = httpClient.execute(post);
-			InputStream content = response.getEntity().getContent();
-			try {
-				Map json = JsonSerialization.readValue(content, Map.class);
-				Object score = json.get("score");
-				logger.debug("Score: " + score);
-				Object val = json.get("success");
-				success = Boolean.TRUE.equals(val);
-			} finally {
-				content.close();
-			}
-		} catch (Exception e) {
-			logger.error("Recaptcha validation failed");
-			ServicesLogger.LOGGER.recaptchaFailed(e);
-		}
-		logger.info("Recaptcha validation successful");
-		return success;
 	}
 
 }
--- src/main/java/com/identicum/keycloak/recaptcha/RecaptchaUsernamePasswordFormFactory.java
+++ src/main/java/com/identicum/keycloak/recaptcha/RecaptchaUsernamePasswordFormFactory.java
@@ -2,6 +2,8 @@ package com.identicum.keycloak.recaptcha;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Timer;
 
 import org.jboss.logging.Logger;
 import org.keycloak.Config;
@@ -16,38 +18,43 @@ import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.models.credential.PasswordCredentialModel;
 import org.keycloak.provider.ProviderConfigProperty;
 
-public class RecaptchaUsernamePasswordFormFactory  implements AuthenticatorFactory, DisplayTypeAuthenticatorFactory {
+import static com.identicum.keycloak.recaptcha.HttpStats.TO_MILLISECONDS;
+import static com.identicum.keycloak.recaptcha.RestConfiguration.*;
 
-    public static final String PROVIDER_ID = "recaptcha-u-p-form";
-    public static final RecaptchaUsernamePasswordForm SINGLETON = new RecaptchaUsernamePasswordForm();
+public class RecaptchaUsernamePasswordFormFactory implements AuthenticatorFactory, DisplayTypeAuthenticatorFactory {
+
+    private static final String PROVIDER_ID = "recaptcha-u-p-form";
     private static final Logger logger = Logger.getLogger(RecaptchaUsernamePasswordFormFactory.class);
+
+    private static Map<String, String> lastConfiguration;
+    private static Timer httpStats;
+
+    protected static RestHandler restHandler;
 
     @Override
     public Authenticator create(KeycloakSession session) {
-        return SINGLETON;
+        return new RecaptchaUsernamePasswordForm();
     }
 
     @Override
     public Authenticator createDisplay(KeycloakSession session, String displayType) {
-        if (displayType == null) return SINGLETON;
+        if (displayType == null) return new RecaptchaUsernamePasswordForm();
         if (!OAuth2Constants.DISPLAY_CONSOLE.equalsIgnoreCase(displayType)) return null;
         return ConsoleUsernamePasswordAuthenticator.SINGLETON;
     }
 
     @Override
     public void init(Config.Scope config) {
-        logger.info("Package version: " + getClass().getPackage().getImplementationVersion());
+        logger.infov("Initializing recaptcha username password form factory version: " + getClass().getPackage().getImplementationVersion());
+        httpStats = new Timer();
     }
 
     @Override
     public void postInit(KeycloakSessionFactory factory) {
-
     }
 
     @Override
-    public void close() {
-
-    }
+    public void close() {}
 
     @Override
     public String getId() {
@@ -65,7 +72,7 @@ public class RecaptchaUsernamePasswordFormFactory  implements AuthenticatorFacto
     }
     
     public static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
-            AuthenticationExecutionModel.Requirement.REQUIRED
+        AuthenticationExecutionModel.Requirement.REQUIRED
     };
 
     @Override
@@ -81,6 +88,16 @@ public class RecaptchaUsernamePasswordFormFactory  implements AuthenticatorFacto
     @Override
     public String getHelpText() {
         return "Validates a username and password from login form + google recaptcha";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return CONFIG_PROPERTIES;
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return false;
     }
 
 	private static final List<ProviderConfigProperty> CONFIG_PROPERTIES = new ArrayList<>();
@@ -100,16 +117,67 @@ public class RecaptchaUsernamePasswordFormFactory  implements AuthenticatorFacto
         property.setType(ProviderConfigProperty.STRING_TYPE);
         property.setHelpText("Google Recaptcha Secret");
         CONFIG_PROPERTIES.add(property);
+
+        property = new ProviderConfigProperty();
+        property.setName(MAX_HTTP_CONNECTIONS);
+        property.setLabel("Max pool connections");
+        property.setType(ProviderConfigProperty.STRING_TYPE);
+        property.setHelpText("Max http connections in pool");
+        property.setDefaultValue(5);
+        CONFIG_PROPERTIES.add(property);
+
+        property = new ProviderConfigProperty();
+        property.setName(API_SOCKET_TIMEOUT);
+        property.setLabel("API Socket Timeout");
+        property.setType(ProviderConfigProperty.STRING_TYPE);
+        property.setHelpText("Max time [milliseconds] to wait for response");
+        property.setDefaultValue(1000);
+        CONFIG_PROPERTIES.add(property);
+
+        property = new ProviderConfigProperty();
+        property.setName(API_CONNECT_TIMEOUT);
+        property.setLabel("API Connect Timeout");
+        property.setType(ProviderConfigProperty.STRING_TYPE);
+        property.setHelpText("Max time [milliseconds] to establish the connection");
+        property.setDefaultValue(1000);
+        CONFIG_PROPERTIES.add(property);
+
+        property = new ProviderConfigProperty();
+        property.setName(API_CONNECTION_REQUEST_TIMEOUT);
+        property.setLabel("API Connection Request Timeout");
+        property.setType(ProviderConfigProperty.STRING_TYPE);
+        property.setHelpText("Max time [milliseconds] to wait until a connection in the pool is assigned to the requesting thread");
+        property.setDefaultValue(1000);
+        CONFIG_PROPERTIES.add(property);
+
+        property = new ProviderConfigProperty();
+        property.setName(HTTP_STATS_INTERVAL);
+        property.setLabel("HttpStats interval");
+        property.setType(ProviderConfigProperty.STRING_TYPE);
+        property.setHelpText("How often [seconds] will the HTTP connection pool stats be displayed. 0 means disabled.");
+        property.setDefaultValue(0);
+        CONFIG_PROPERTIES.add(property);
     }
 
-	@Override
-	public List<ProviderConfigProperty> getConfigProperties() {
-		return CONFIG_PROPERTIES;
-	}
+    private static void setHttpStats(Logger logger, RestHandler restHandler, Integer httpStatsInterval){
+        httpStats.cancel();
+        if(httpStatsInterval > 0){
+            httpStats = new Timer();
+            httpStats.schedule(new HttpStats(logger, restHandler), 0, httpStatsInterval * TO_MILLISECONDS);
+        }
+    }
 
-    @Override
-    public boolean isUserSetupAllowed() {
-        return false;
+    protected static void createRestHandlerAndSetStats(Map<String, String> configuration){
+        if(restHandler == null || !configuration.equals(lastConfiguration)){
+            logger.infov("Creating a new instance of restHandler");
+            RestConfiguration restConfiguration = new RestConfiguration(configuration);
+            restHandler = new RestHandler(restConfiguration);
+            setHttpStats(logger, restHandler, restConfiguration.getHttpStatsInterval());
+            lastConfiguration = configuration;
+        }
+        else {
+            logger.infov("RestHandler already instantiated");
+        }
     }
 
 }
